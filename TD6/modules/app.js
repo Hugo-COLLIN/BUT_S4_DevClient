586c617313d31d584233cb77bbdd66b772612d31
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-05-10 10:43:59 +0200

    TD6 ex5 search ok

--- TD6/modules/app.js
+++ TD6/modules/app.js
@@ -3,4 +3,11 @@ import ui from "./ui.js";
 
 export const init = () => {
   ui.buildProductList(products.list);
+
+  document.addEventListener('keyup', (event) => {
+    document.querySelector('#product-list').innerHTML = '';
+    const keywords = event.target.value;
+    const productsList = products.search(keywords);
+    ui.buildProductList(productsList);
+  });
 }
--- TD6/modules/products.js
+++ TD6/modules/products.js
@@ -1,4 +1,4 @@
-let productsList = [
+const productsList = [
     { ref: "REF1", price: "250,29" , desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
     { ref: "REF2", price: "129,25", desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
     { ref: "REF3", price: "18,10", desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
@@ -7,6 +7,15 @@ let productsList = [
     { ref: "REF6", price: "50,35", desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
   ];
 
+const search = (keywords) => {
+  const matchKeywords = element => element.ref.includes(keywords) || element.desc.includes(keywords) || element.price.includes(keywords);
+  let result = productsList.filter(matchKeywords);
+  console.log(result);
+  console.log(keywords);
+  return result;
+}
+
 export default {
   list: productsList,
-}
+  search: search
+};
