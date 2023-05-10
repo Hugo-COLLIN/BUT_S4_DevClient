5d80f3b77d8f21797a07351816a99c1c8654b109
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-05-10 10:55:49 +0200

    TD6 ex6

---
+++ TD6/modules/cart.js
@@ -0,0 +1,12 @@
+const cartContent = [];
+
+let addToCart = (product) => {
+  const matchCart = (elt) => elt.productName === product;
+  let res = cartContent.filter(matchCart);
+  res.length === 0 ? cartContent.push({productName: product, qty: 1}) : res[0].qty ++ ;
+  console.log(cartContent);
+}
+
+export default {
+  addToCart: addToCart
+}
