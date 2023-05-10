6b55578a71e4e31ef72a7082e7f0315c3cab2f5f
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-05-10 09:05:09 +0200

    TD6 ex1

---
+++ TD6/modules/products.js
@@ -0,0 +1,12 @@
+let productsList = [
+    { ref: 1, desc: 'Product 1', price: 100 },
+    { ref: 2, desc: 'Product 2', price: 200 },
+    { ref: 3, desc: 'Product 3', price: 300 },
+    { ref: 4, desc: 'Product 4', price: 400 },
+    { ref: 5, desc: 'Product 5', price: 500 },
+    { ref: 6, desc: 'Product 6', price: 600 },
+  ];
+
+export default {
+  products: productsList,
+}
