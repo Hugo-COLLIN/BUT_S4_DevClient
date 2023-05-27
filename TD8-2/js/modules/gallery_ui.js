ed51d6901731f46d0cf3f9d25b8b9cd2ac943040
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-05-27 23:48:13 +0200

    TD8 ex4 display image when click on thumbnail

--- TD8-2/js/modules/gallery_ui.js
+++ TD8-2/js/modules/gallery_ui.js
@@ -5,16 +5,27 @@ function displayGallery(gallery) {
   photosElt.innerHTML = "";
   console.log(photosElt);
   gallery.photos.forEach(photo => {
-    console.log(photo)
-    photosElt.innerHTML += `
-    <div class="vignette" >
-      <img data-photoId = "${photo.photo.id}" src="${config.URLbase + photo.photo.thumbnail.href}">
-    </div>
-    `;
-    photosElt.addEventListener("click", () => {
-      console.log(photo.links.self.href)
+    console.log(photo);
+
+    const vignetteDiv = document.createElement("div");
+    vignetteDiv.classList.add("vignette");
+
+    const imgLink = document.createElement("a");
+    imgLink.href = "#image-container";
+
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("data-photoId", photo.photo.id);
+    imgElement.src = config.URLbase + photo.photo.thumbnail.href;
+
+    imgElement.addEventListener("click", () => {
+      console.log(photo.links.self.href);
       document.querySelector("#image-view > img").src = config.URLbase + photo.photo.original.href;
+      document.querySelector("#image-container > h2").innerHTML = photo.photo.titre;
     });
+
+    vignetteDiv.appendChild(imgLink);
+    imgLink.appendChild(imgElement);
+    photosElt.appendChild(vignetteDiv);
   });
 }
 
